e5039b808cbad2cb5080aa62cb423f8c8e077946
Jose Curti <joc_qr@hotmail.com>
2019-03-19 10:20:03 -0300

    Devolucion Contado y Credito
    Creacion de clases para las devoluciones y acomodada la funcion facturar en base al tipo de documento.

---
+++ Entidades/Documentos/Devoluciones/DevolucionContado.cs
@@ -0,0 +1,65 @@
+﻿using JJ.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JJ.Entidades
+{
+    public class DevolucionContado : Factura
+    {
+        private ClienteContado _Cliente;
+        private String _SerieReferencia;
+        private int _NumeroReferencia;
+
+        public string SerieReferencia
+        {
+            get
+            {
+                return _SerieReferencia;
+            }
+
+            set
+            {
+                _SerieReferencia = value;
+            }
+        }
+
+        public int NumeroReferencia
+        {
+            get
+            {
+                return _NumeroReferencia;
+            }
+
+            set
+            {
+                _NumeroReferencia = value;
+            }
+        }
+
+        public ClienteContado Cliente
+        {
+            get
+            {
+                return _Cliente;
+            }
+
+            set
+            {
+                _Cliente = value;
+            }
+        }
+
+        public DevolucionContado(int xNumero, string xSerie, string xCodCaja, DateTime xFecha, int xCodMoneda, int xZ, int xcodvendedor, int xcoddocumento, string xSerieReferencia, int xNumeroReferencia, ClienteContado xCliente) : base(xNumero, xSerie, xCodCaja, xFecha, xCodMoneda, xZ, xcodvendedor, xcoddocumento)
+        {
+            _Cliente = xCliente;
+            _SerieReferencia = xSerieReferencia;
+            _NumeroReferencia = xNumeroReferencia;
+        }
+
+       
+    }
+}
+
+
---
+++ Entidades/Documentos/Devoluciones/DevolucionCuenta.cs
@@ -0,0 +1,94 @@
+﻿using JJ.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JJ.Entidades
+{
+    public class DevolucionCuenta : Factura
+    {
+        private int _CodCLiente;
+        private Cuenta _Cuenta;
+        private int _CodTarifa;
+        private String _SerieReferencia;
+        private int _NumeroReferencia;
+
+        public string SerieReferencia
+        {
+            get
+            {
+                return _SerieReferencia;
+            }
+
+            set
+            {
+                _SerieReferencia = value;
+            }
+        }
+
+        public int NumeroReferencia
+        {
+            get
+            {
+                return _NumeroReferencia;
+            }
+
+            set
+            {
+                _NumeroReferencia = value;
+            }
+        }
+
+        public int CodCLiente
+        {
+            get
+            {
+                return _CodCLiente;
+            }
+
+            set
+            {
+                _CodCLiente = value;
+            }
+        }
+
+        public Cuenta Cuenta
+        {
+            get
+            {
+                return _Cuenta;
+            }
+
+            set
+            {
+                _Cuenta = value;
+            }
+        }
+
+        public int CodTarifa
+        {
+            get
+            {
+                return _CodTarifa;
+            }
+
+            set
+            {
+                _CodTarifa = value;
+            }
+        }
+
+        public DevolucionCuenta(int xNumero, string xSerie, string xCodCaja, DateTime xFecha, int xCodMoneda, int xZ, int xcodvendedor, int xcoddocumento, Cuenta xCuenta, int xCodCliente, int xCodTarifa, string xSerieReferencia, int xNumeroReferencia) : base(xNumero, xSerie, xCodCaja, xFecha, xCodMoneda, xZ, xcodvendedor, xcoddocumento)
+        {
+            _CodCLiente = xCodCliente;
+            _Cuenta = xCuenta;
+            _CodTarifa = xCodTarifa;
+            _SerieReferencia = xSerieReferencia;
+            _NumeroReferencia = xNumeroReferencia;
+        }
+
+
+    }
+}
+
